8fefc78ac0141f5b81a6f63344c5440daf8cc6d7
Somi-Kim <kimsnack0604@naver.com>
2021-09-06 08:37:27 +0900

    ~#4.5

--- src/App.js
+++ src/App.js
@@ -1,21 +1,53 @@
 import React from 'react';
 // import Potato from './Potato'; // 경로에서 >>>>> ./ <<<<< 는 같은 directory를 의미
 // import PropTypes from "prop-types";
+import axios from "axios";
+import Movie from './Movie'
+import "./App.css"
 
 class App extends React.Component {
   state = {
     isLoading: true,
     movies: []
   };
+  getMovies = async () => {
+    const {
+      data: {
+        data: {movies}
+      }
+    } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+    // const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    // console.log(movies);
+    this.setState({ movies, isLoading: false }); // this.setState({ movies : movies });
+  }
   componentDidMount() {
-    
+    this.getMovies()
   }
   render() {
-    const { isLoading } = this.state;
+    const { isLoading, movies } = this.state;
+    // console.log(movies);
     return (
-      <div>
-        {isLoading ? "Loading" : "We are ready"}    
-      </div>
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text">Loading. . .</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map(movie => (
+              <Movie 
+                key={movie.id}
+                id={movie.id} 
+                year={movie.year} 
+                title={movie.title} 
+                summary={movie.summary} 
+                poster={movie.medium_cover_image} 
+                genres={movie.genres}
+              />
+            ))}
+          </div>
+        )}    
+      </section>
       )
   }
 }
---
+++ src/Movie.js
@@ -0,0 +1,38 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./Movie.css"
+
+//state가 필요 없는 경우 class component일 필요 없이 function component 이용
+function Movie({ year, title, summary, poster, genres }) {
+    return (
+        <div className="movies__movie">
+            <img src={poster} alt={title} title={title} />
+            <div className="movie__data">
+                <h3 className="movie__title">{title}</h3>
+                <h5 className="movie__year">{year}</h5>
+                <ul className="genres">
+                    {genres.map((genre, index) => (
+                        <li key={index} className="genres__genre">{genre}</li>
+                    ))}
+                </ul>
+                <p className="movie__summary">
+                    {summary.length <= 300 ? 
+                        summary 
+                    : 
+                        `${summary.slice(0, 300)}...`
+                    }
+                </p>
+            </div>
+        </div>)
+}
+
+Movie.propTypes = {
+    id: PropTypes.number.isRequired,
+    year: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    summary: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired,
+    genres: PropTypes.arrayOf(PropTypes.string).isRequired
+}
+
+export default Movie;
